bbd75b1b0fd223f6d8d43bb04044abab5bdf97e2
Jens Reimann <jens.reimann@ibh-systems.com>
2015-12-16 12:14:05 +0100

    add a versioned product name and some explanations

--- bundles/org.eclipse.packagedrone/src/org/eclipse/packagedrone/VersionInformation.java
+++ bundles/org.eclipse.packagedrone/src/org/eclipse/packagedrone/VersionInformation.java
@@ -15,10 +15,43 @@ import org.osgi.framework.Version;
 
 public final class VersionInformation
 {
+    /**
+     * The product version as string
+     */
     public static final String VERSION;
 
+    /**
+     * The product version as unqualified string
+     * <p>
+     * This is the same version string as {@link #VERSION} but without the build
+     * qualifier (4th component).
+     * </p>
+     */
     public static final String VERSION_UNQUALIFIED;
 
+    /**
+     * The name of the product.
+     * <p>
+     * This is "Eclipse Package Drone".
+     * </p>
+     */
+    public static final String PRODUCT = "Eclipse Package Drone";
+
+    /**
+     * The product name in combination with the version
+     * <p>
+     * This is {@code PRODUCT + " " + VERSION}
+     * </p>
+     */
+    public static final String VERSIONED_PRODUCT;
+
+    /**
+     * A string which can be used as HTTP user agent
+     * <p>
+     * It contains the product name in a suitable format, the URL and the
+     * unqualified version.
+     * </p>
+     */
     public static final String USER_AGENT;
 
     private VersionInformation ()
@@ -30,6 +63,8 @@ public final class VersionInformation
         final Version version = FrameworkUtil.getBundle ( VersionInformation.class ).getVersion ();
         VERSION = version.toString ();
         VERSION_UNQUALIFIED = new Version ( version.getMajor (), version.getMinor (), version.getMicro () ).toString ();
+
+        VERSIONED_PRODUCT = PRODUCT + " " + VERSION;
 
         USER_AGENT = String.format ( "PackageDrone/%s (+http://packagedrone.org)", VersionInformation.VERSION_UNQUALIFIED );
     }
