0e8cd74907c9e2d64e2045ca5d34d6a110274e8c
Jens Reimann <jens.reimann@ibh-systems.com>
2016-02-11 16:27:52 +0100

    clear warning

--- bundles/org.eclipse.packagedrone.repo.adapter.r5/src/org/eclipse/packagedrone/repo/adapter/r5/internal/handler/NotFoundHandler.java
+++ bundles/org.eclipse.packagedrone.repo.adapter.r5/src/org/eclipse/packagedrone/repo/adapter/r5/internal/handler/NotFoundHandler.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014, 2015 IBH SYSTEMS GmbH.
+ * Copyright (c) 2014, 2016 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -32,7 +32,6 @@ public class NotFoundHandler implements Handler
     {
         resp.setStatus ( HttpServletResponse.SC_NOT_FOUND );
 
-        @SuppressWarnings ( "resource" )
         final PrintWriter w = resp.getWriter ();
         resp.setContentType ( "text/plain" );
 
