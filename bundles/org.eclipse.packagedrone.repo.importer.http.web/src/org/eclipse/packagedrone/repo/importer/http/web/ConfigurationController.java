8410363ce271bd965e6a4dafedd1a3a3271797c5
Jens Reimann <jens.reimann@ibh-systems.com>
2016-03-08 09:15:44 +0100

    fix NPE

--- bundles/org.eclipse.packagedrone.repo.importer.http.web/src/org/eclipse/packagedrone/repo/importer/http/web/ConfigurationController.java
+++ bundles/org.eclipse.packagedrone.repo.importer.http.web/src/org/eclipse/packagedrone/repo/importer/http/web/ConfigurationController.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * Copyright (c) 2015, 2016 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -60,7 +60,8 @@ public class ConfigurationController
     }
 
     @RequestMapping ( value = "/import/{token}/http/start", method = RequestMethod.GET )
-    public ModelAndView configure ( @RequestParameter ( value = "configuration", required = false ) final Configuration cfg )
+    public ModelAndView configure ( @RequestParameter ( value = "configuration",
+            required = false ) final Configuration cfg )
     {
         final Map<String, Object> model = new HashMap<> ();
 
@@ -72,6 +73,8 @@ public class ConfigurationController
         {
             model.put ( "command", new Configuration () );
         }
+
+        model.put ( "ok", Boolean.FALSE );
 
         return new ModelAndView ( "configure", model );
     }
