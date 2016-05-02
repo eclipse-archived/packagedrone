0709fa83ed6631f45e2240e1a5804439448a3c50
Jens Reimann <jens.reimann@ibh-systems.com>
2016-05-02 12:33:47 +0200

    added a few more tags

--- bundles/org.eclipse.packagedrone.utils.rpm/src/org/eclipse/packagedrone/utils/rpm/RpmSignatureTag.java
+++ bundles/org.eclipse.packagedrone.utils.rpm/src/org/eclipse/packagedrone/utils/rpm/RpmSignatureTag.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * Copyright (c) 2015, 2016 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,10 +15,13 @@ import java.util.Map;
 
 public enum RpmSignatureTag implements RpmBaseTag
 {
+    PUBKEYS ( 266 ),
+    RSAHEADER ( 268 ),
     SHA1HEADER ( 269 ),
     LONGARCHIVESIZE ( 271 ),
 
     SIZE ( 1000 ),
+    PGP ( 1002 ),
     MD5 ( 1004 ),
     PAYLOAD_SIZE ( 1007 ),
     LONGSIZE ( 5009 );
