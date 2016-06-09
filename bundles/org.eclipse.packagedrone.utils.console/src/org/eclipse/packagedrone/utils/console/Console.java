2db428c25437514b1f7bf88ca0d53e222d598e7d
Jens Reimann <jens.reimann@ibh-systems.com>
2016-06-09 13:01:28 +0200

    element console command to get configuration

--- bundles/org.eclipse.packagedrone.utils.console/src/org/eclipse/packagedrone/utils/console/Console.java
+++ bundles/org.eclipse.packagedrone.utils.console/src/org/eclipse/packagedrone/utils/console/Console.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * Copyright (c) 2015, 2016 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,9 +10,11 @@
  *******************************************************************************/
 package org.eclipse.packagedrone.utils.console;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -36,12 +38,42 @@ public class Console
 
     public void allConfigs () throws Exception
     {
-        dump ( Console.this.configAdmin.listConfigurations ( null ) );
+        dump ( this.configAdmin.listConfigurations ( null ) );
     }
 
     public void listConfigs ( final String filter ) throws Exception
     {
-        dump ( Console.this.configAdmin.listConfigurations ( filter ) );
+        dump ( this.configAdmin.listConfigurations ( filter ) );
+    }
+
+    public void getConfig ( final String pid ) throws IOException
+    {
+        final Configuration cfg = this.configAdmin.getConfiguration ( pid );
+        if ( cfg == null )
+        {
+            System.out.println ( "Not found" );
+            return;
+        }
+
+        System.out.format ( "Location: %s%n", cfg.getBundleLocation () );
+        System.out.format ( "Factory PID: %s%n", cfg.getFactoryPid () );
+        System.out.format ( "PID: %s%n", cfg.getPid () );
+
+        if ( cfg.getProperties () != null )
+        {
+            final List<List<String>> data = new LinkedList<> ();
+
+            final Enumeration<String> en = cfg.getProperties ().keys ();
+            while ( en.hasMoreElements () )
+            {
+                final String key = en.nextElement ();
+                final Object value = cfg.getProperties ().get ( key );
+
+                data.add ( Arrays.asList ( key, "" + value ) );
+            }
+
+            Tables.showTable ( System.out, Arrays.asList ( "Key", "Value" ), data, 2 );
+        }
     }
 
     private void dump ( final Configuration[] cfgs )
