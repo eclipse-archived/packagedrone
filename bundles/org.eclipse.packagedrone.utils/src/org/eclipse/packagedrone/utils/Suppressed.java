317602af99254d8872b99d6ca9b10e2d088624c7
Jens Reimann <jens.reimann@ibh-systems.com>
2016-06-23 14:45:25 +0200

    don't fail when the closable is null

--- bundles/org.eclipse.packagedrone.utils/src/org/eclipse/packagedrone/utils/Suppressed.java
+++ bundles/org.eclipse.packagedrone.utils/src/org/eclipse/packagedrone/utils/Suppressed.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * Copyright (c) 2015, 2016 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -39,6 +39,11 @@ public class Suppressed<T extends Exception> implements AutoCloseable
 
     public void close ( final Closeable closeable )
     {
+        if ( closeable == null )
+        {
+            return;
+        }
+
         try
         {
             closeable.close ();
