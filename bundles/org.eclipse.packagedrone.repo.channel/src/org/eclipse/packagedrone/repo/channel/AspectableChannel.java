5d03f5a45473c31fbe50744943c10a35732c3e3f
Jens Reimann <jens.reimann@ibh-systems.com>
2016-06-24 11:21:43 +0200

    allow using sets as well

--- bundles/org.eclipse.packagedrone.repo.channel/src/org/eclipse/packagedrone/repo/channel/AspectableChannel.java
+++ bundles/org.eclipse.packagedrone.repo.channel/src/org/eclipse/packagedrone/repo/channel/AspectableChannel.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 IBH SYSTEMS GmbH.
+ * Copyright (c) 2015, 2016 IBH SYSTEMS GmbH.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,11 +10,32 @@
  *******************************************************************************/
 package org.eclipse.packagedrone.repo.channel;
 
+import java.util.Objects;
+import java.util.Set;
+
 public interface AspectableChannel
 {
     public void addAspects ( boolean withDependencies, String... aspectIds );
 
+    public default void addAspects ( final boolean withDependencies, final Set<String> aspectIds )
+    {
+        Objects.requireNonNull ( aspectIds );
+        addAspects ( withDependencies, aspectIds.toArray ( new String[aspectIds.size ()] ) );
+    }
+
     public void removeAspects ( String... aspectIds );
 
+    public default void removeAspects ( final Set<String> aspectIds )
+    {
+        Objects.requireNonNull ( aspectIds );
+        removeAspects ( aspectIds.toArray ( new String[aspectIds.size ()] ) );
+    }
+
     public void refreshAspects ( String... aspectIds );
+
+    public default void refreshAspects ( final Set<String> aspectIds )
+    {
+        Objects.requireNonNull ( aspectIds );
+        refreshAspects ( aspectIds.toArray ( new String[aspectIds.size ()] ) );
+    }
 }
