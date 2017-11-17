2a024f4007ca31b4d47a724a0b55b44f728ba964
Peter Jeschke <looperhacks+eclipse@gmail.com>
2017-11-17 13:10:58 +0100

    Extract all entries from the Manifest
    This addition adds all keys from the Manifest file as metadata to be
    used for the cleaner aspect.
    
    Signed-off-by: Peter Jeschke <looperhacks+eclipse@gmail.com>

--- bundles/org.eclipse.packagedrone.repo.aspect.common/src/org/eclipse/packagedrone/repo/aspect/common/osgi/OsgiExtractor.java
+++ bundles/org.eclipse.packagedrone.repo.aspect.common/src/org/eclipse/packagedrone/repo/aspect/common/osgi/OsgiExtractor.java
@@ -15,6 +15,7 @@ import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
@@ -125,6 +126,8 @@ public class OsgiExtractor implements Extractor
                 manifest = new Manifest ( is );
             }
 
+            storeManifestData ( manifest, metadata );
+
             try
             {
                 bi = new BundleInformationParser ( zipFile, manifest ).parse ();
@@ -173,6 +176,14 @@ public class OsgiExtractor implements Extractor
         // store bundle information
         metadata.put ( KEY_NAME_BUNDLE_INFORMATION, bi.toJson () );
 
+    }
+
+    private void storeManifestData ( final Manifest manifest, final Map<String, String> metadata )
+    {
+        for ( final Entry<Object, Object> entry : manifest.getMainAttributes ().entrySet () )
+        {
+            metadata.put ( ( (Attributes.Name)entry.getKey () ).toString (), (String)entry.getValue () );
+        }
     }
 
     private boolean validateBundle ( final Context context, final BundleInformation bi )
