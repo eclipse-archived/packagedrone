0ba76062cd2bd32a45ed94cab03bf856e5ea2094
Jens Reimann <jreimann@redhat.com>
2018-04-16 10:59:07 +0200

    Allow mapping arch/os in a custom way

--- bundles/org.eclipse.packagedrone.utils.rpm.tests/src/org/eclipse/packagedrone/utils/rpm/tests/LeadTest.java
+++ bundles/org.eclipse.packagedrone.utils.rpm.tests/src/org/eclipse/packagedrone/utils/rpm/tests/LeadTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2016 IBH SYSTEMS GmbH and others.
+ * Copyright (c) 2016, 2018 IBH SYSTEMS GmbH and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,12 +7,19 @@
  *
  * Contributors:
  *     IBH SYSTEMS GmbH - initial API and implementation
+ *     Red Hat Inc - test lead mappers
  *******************************************************************************/
 package org.eclipse.packagedrone.utils.rpm.tests;
+
+import static java.util.Optional.of;
 
 import java.util.Optional;
 
 import org.eclipse.packagedrone.utils.rpm.Architecture;
+import org.eclipse.packagedrone.utils.rpm.OperatingSystem;
+import org.eclipse.packagedrone.utils.rpm.RpmTag;
+import org.eclipse.packagedrone.utils.rpm.build.LeadBuilder;
+import org.eclipse.packagedrone.utils.rpm.header.Header;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -37,5 +44,28 @@ public class LeadTest
         {
             Assert.assertEquals ( expected, arch.orElse ( null ) );
         }
+    }
+
+    /**
+     * Test the mappers for arch and os.
+     */
+    @Test
+    public void testMapper1 ()
+    {
+        final LeadBuilder lead = new LeadBuilder ();
+        final Header<RpmTag> header = new Header<> ();
+
+        header.putString ( RpmTag.ARCH, "foo-bar" );
+        header.putString ( RpmTag.OS, "bar-foo" );
+
+        lead.fillFlagsFromHeader ( header );
+
+        Assert.assertEquals ( Architecture.NOARCH.getValue (), lead.getArchitecture () );
+        Assert.assertEquals ( OperatingSystem.UNKNOWN.getValue (), lead.getOperatingSystem () );
+
+        lead.fillFlagsFromHeader ( header, s -> of ( Architecture.ARM ), s -> of ( OperatingSystem.AIX ) );
+
+        Assert.assertEquals ( Architecture.ARM.getValue (), lead.getArchitecture () );
+        Assert.assertEquals ( OperatingSystem.AIX.getValue (), lead.getOperatingSystem () );
     }
 }
