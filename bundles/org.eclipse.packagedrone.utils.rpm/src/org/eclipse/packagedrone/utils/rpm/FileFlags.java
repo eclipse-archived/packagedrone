23ba4bd7ba6f25e78307a580e0e952f882f89e27
Jens Reimann <jreimann@redhat.com>
2018-06-04 15:03:15 +0200

    Minor cleanups

--- bundles/org.eclipse.packagedrone.utils.rpm/src/org/eclipse/packagedrone/utils/rpm/FileFlags.java
+++ bundles/org.eclipse.packagedrone.utils.rpm/src/org/eclipse/packagedrone/utils/rpm/FileFlags.java
@@ -15,17 +15,17 @@ import java.util.EnumSet;
 
 public enum FileFlags
 {
-    CONFIGURATION ( 1 <<  0 ), /*!< from %%config */
-    DOC ( 1 <<  1 ), /*!< from %%doc */
-    ICON ( 1 <<  2 ), /*!< from %%donotuse. */
-    MISSINGOK ( 1 <<  3 ), /*!< from %%config(missingok) */
-    NOREPLACE ( 1 <<  4 ), /*!< from %%config(noreplace) */
-    GHOST ( 1 <<  6 ), /*!< from %%ghost */
-    LICENSE ( 1 <<  7 ), /*!< from %%license */
-    README ( 1 <<  8 ), /*!< from %%readme */
+    CONFIGURATION ( 1 << 0 ), /*!< from %%config */
+    DOC ( 1 << 1 ), /*!< from %%doc */
+    ICON ( 1 << 2 ), /*!< from %%donotuse. */
+    MISSINGOK ( 1 << 3 ), /*!< from %%config(missingok) */
+    NOREPLACE ( 1 << 4 ), /*!< from %%config(noreplace) */
+    GHOST ( 1 << 6 ), /*!< from %%ghost */
+    LICENSE ( 1 << 7 ), /*!< from %%license */
+    README ( 1 << 8 ), /*!< from %%readme */
     /* bits 9-10 unused */
     PUBKEY ( 1 << 11 ), /*!< from %%pubkey */
-    ARTIFACT ( 1 << 12 );	/*!< from %%artifact */
+    ARTIFACT ( 1 << 12 ); /*!< from %%artifact */
 
     private int value;
 
@@ -39,16 +39,16 @@ public enum FileFlags
         return this.value;
     }
 
-    public static EnumSet<FileFlags> decode( int flagValue )
+    public static EnumSet<FileFlags> decode ( final int flagValue )
     {
-        EnumSet<FileFlags> fileFlags = EnumSet.noneOf( FileFlags.class );
+        final EnumSet<FileFlags> fileFlags = EnumSet.noneOf ( FileFlags.class );
         if ( flagValue != 0 )
         {
-            for ( FileFlags fileFlag : FileFlags.values() )
+            for ( final FileFlags fileFlag : FileFlags.values () )
             {
-                if ( ( fileFlag.getValue() & flagValue ) == fileFlag.getValue() )
+                if ( ( fileFlag.getValue () & flagValue ) == fileFlag.getValue () )
                 {
-                    fileFlags.add(fileFlag);
+                    fileFlags.add ( fileFlag );
                 }
             }
         }
